5f312a7a6c5289e5ac63f5c695edaf74169803ea
Leroy <leroypawlick@gmail.com>
2023-03-31 09:16:43 -0700

    added 2 unit tests

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -1,9 +1,10 @@
-const { Builder, Browser, By, until } = require("selenium-webdriver");
+const { Builder, Browser, By, until, Capabilities } = require("selenium-webdriver");
+require('chromedriver')
 
 let driver;
 
 beforeEach(async () => {
-  driver = await new Builder().forBrowser(Browser.CHROME).build();
+  driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 });
 
 afterEach(async () => {
@@ -15,4 +16,11 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  // test("draw button should display div", async () => {
+  //   let drawBtn = await driver.findElement(By.id("draw"));
+  //   await drawBtn.click();
+  //   await driver.findElement(By.xpath('//*[@id="choices"]'));
+
+  // })
 });
--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,11 @@
-const { shuffle } = require('../src/shuffle')
+const shuffle = require('../src/shuffle')
 
 describe('shuffle should...', () => {
-  // CODE HERE
-})
+  test('shuffle should return an array', () => {
+    expect(shuffle([])).toStrictEqual([])
+  });
+
+  test('check array length to be the same', () => {
+    expect(shuffle([1, 2, 3])).toHaveLength(3);
+  })
+});
